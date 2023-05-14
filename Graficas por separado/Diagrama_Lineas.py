8288b40d3dba949986f9370c09f141fd710e2382
John Elvis Arbaiza Velásquez <102486877+JohnArbaiza1@users.noreply.github.com>
2023-05-14 15:05:19 -0600

    Delete Graficas por separado directory

--- Graficas por separado/Diagrama_Lineas.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-#plot(x, y): Dibuja un polígono con los vértices dados por las coordenadas de la
-#lista x en el eje X y las coordenadas de la lista y en el eje Y
-fig, ax = plt.subplots()
-ax.plot([1,2,3,4,5],[1,2,0,0,5], color='yellow')
-plt.show()
--- Graficas por separado/Grafico_Pastel.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-#pie(x): Dibuja un diagrama de sectores con las frecuencias de la lista x.
-fig , ax =plt.subplots()
-ax.pie([10,34,67,12])
-plt.show()
